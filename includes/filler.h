ba66feb7de9ae9fc6674ff72efd46ed827e22eda
Mathys Taquet <mtaquet@z2r7p6.le-101.fr>
2019-01-11 14:55:42 +0100

    struct

--- includes/filler.h
+++ includes/filler.h
@@ -1,1 +1,39 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   filler.h                                         .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: mtaquet <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/01/11 14:37:41 by mtaquet      #+#   ##    ##    #+#       */
+/*   Updated: 2019/01/11 14:50:42 by mtaquet     ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
 
+#ifndef	FILLER_H
+# define FILLER_H
+
+# include "libft.h"
+
+struct				s_point
+{
+	int x;
+	int y;
+};
+
+typedef struct s_point t_point;
+
+
+
+struct				s_map
+{
+	char			**map;
+	t_point			size;
+	char			player;
+};
+
+typedef struct s_map t_map;
+
+
+#endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,33 +1,48 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   main.c                                           .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: mtaquet <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/01/11 14:49:02 by mtaquet      #+#   ##    ##    #+#       */
+/*   Updated: 2019/01/11 14:49:53 by mtaquet     ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "filler.h"
+
 int main(void)
 {
-	char *line;
-	char player;
-	int mapx;
-	int mapy;
-	char **map;
-	int n;
+	t_map	*map;
+	char	*line;
+	int		n;
 
+	if (!(map = malloc(sizeof(t_map))))
+		return (0);
 	n = -1;
 	get_next_line(0, &line);
 	if (line[10] == '1')
-		player = 'o';
+		map->player = 'o';
 	else if (line[10] == '2')
-		player = 'x';
+		map->player = 'x';
 	while (ft_strncmp(line, "Plateau", 7))
 	{
 		free(line);
 		get_next_line(0, &line);
 	}
-	mapy = ft_atoi(&(line[8]));
-	mapx = ft_atoi(&(line[11]));
+	map->size.y = ft_atoi(&(line[8]));
+	map->size.x = ft_atoi(&(line[11]));
 	free(line);
 	get_next_line(0, &line);
 	free(line);
-	map = malloc(sizeof(char*) * mapy); //protege
-	while (++n < mapy)
+	if (!(map->map = malloc(sizeof(char*) * map->size.y)))
+		return (0);
+	while (++n < map->size.y)
 	{
 		get_next_line(0, &line);
-		map[n] = ft_strdup(&(line[4]));
+		map->map[n] = ft_strdup(&(line[4]));
 		free(line);
 	}
 	return (0);
